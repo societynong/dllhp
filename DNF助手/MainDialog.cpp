f1679f8619eaa7cf04dd93eec1d9707c0bfb684b
zyn <172065208@mail.qq.com>
2020-01-08 21:46:10 +0800

    添加项目文件。

---
+++ DNF助手/MainDialog.cpp
@@ -0,0 +1,34 @@
+﻿// MainDialog.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "DNF助手.h"
+#include "MainDialog.h"
+#include "afxdialogex.h"
+
+
+// MainDialog 对话框
+
+IMPLEMENT_DYNAMIC(MainDialog, CDialog)
+
+MainDialog::MainDialog(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_MainDialog, pParent)
+{
+
+}
+
+MainDialog::~MainDialog()
+{
+}
+
+void MainDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(MainDialog, CDialog)
+END_MESSAGE_MAP()
+
+
+// MainDialog 消息处理程序
---
+++ DNF助手/MainDialog.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// MainDialog 对话框
+
+class MainDialog : public CDialog
+{
+	DECLARE_DYNAMIC(MainDialog)
+
+public:
+	MainDialog(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~MainDialog();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MainDialog };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
